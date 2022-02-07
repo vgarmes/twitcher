938f4a134bd055011ba6c5134e8821b11f3929f0
Victor Garcia Mestre <vgmestre@gmail.com>
2022-02-07 22:13:00 +0100

    api call test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,22 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import { useSession } from 'next-auth/react';
+import axios from 'axios';
 
 const Home: NextPage = () => {
   const { data: session } = useSession();
   if (session) {
+    const url =
+      'https://api.twitch.tv/helix/videos?' +
+      new URLSearchParams({ id: '1288976401' });
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${session.accessToken}`,
+        'Client-Id': `${process.env.NEXT_PUBLIC_TWITCH_CLIENT_ID}`,
+      },
+    };
+    axios.get(url, config).then(console.log).catch(console.log);
     return <div>Signed in as {session.user?.name}</div>;
   }
   console.log(session);
