4173e4fdd5f8dbb55184b3234bcae9cdbfccc565
Victor Garcia Mestre <vgmestre@gmail.com>
2022-02-12 15:37:57 +0100

    get follows

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,23 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
 import { useSession } from 'next-auth/react';
 import axios from 'axios';
-import Layout from '@/layouts/article';
+import Layout from '../layouts/article';
+import { useFollows } from '../hooks/twitch-api';
 
 const Home: NextPage = () => {
   const { data: session } = useSession();
-  if (session) {
-    const url =
-      'https://api.twitch.tv/helix/videos?' +
-      new URLSearchParams({ id: '1288976401' });
+  /*  
+  const config = 
+    {
+        headers: {
+          Authorization: `Bearer ${session?.accessToken}`,
+          'Client-Id': `${process.env.NEXT_PUBLIC_TWITCH_CLIENT_ID}`,
+        },
+      }
+  ; */
+  const { data: follows, error } = useFollows();
+  console.log(follows);
 
-    const config = {
-      headers: {
-        Authorization: `Bearer ${session.accessToken}`,
-        'Client-Id': `${process.env.NEXT_PUBLIC_TWITCH_CLIENT_ID}`,
-      },
-    };
-    axios.get(url, config).then(console.log).catch(console.log);
-    return <div>Signed in as {session.user?.name}</div>;
-  }
-  console.log(session);
   return (
     <Layout>
       <main></main>
