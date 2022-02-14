41c3078c55a4d022703d6ceed61f8fe3195468b9
Victor Garcia Mestre <vgmestre@gmail.com>
2022-02-14 23:23:37 +0100

    followed users list

---
+++ components/Follows.tsx
@@ -0,0 +1,39 @@
+import useSWR from 'swr';
+import { useFollows } from '../hooks/twitch-api';
+import Image from 'next/image';
+
+const Follows = () => {
+  const { data, error } = useFollows();
+  console.log(data);
+  return (
+    <div>
+      <h1 className="uppercase font-bold text-sm p-1.5">Followed</h1>
+      <ul>
+        {data &&
+          data.followed_users
+            .sort((a, b) =>
+              a.display_name < b.display_name
+                ? -1
+                : a.display_name > b.display_name
+                ? 1
+                : 0
+            )
+            .map((user) => (
+              <li key={user.id} className="flex items-center p-1.5">
+                <Image
+                  className="rounded-full px-3"
+                  alt="user profile"
+                  src={user.profile_image_url}
+                  width={30}
+                  height={30}
+                />
+
+                <p className="text-sm pl-2">{user.display_name}</p>
+              </li>
+            ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Follows;
