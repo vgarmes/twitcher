259ac2572af1fb07db58ff6b36651fce0933dd90
Victor Garcia Mestre <vgmestre@gmail.com>
2022-02-22 22:07:48 +0100

    add title, time ago and duration to video card

--- components/CardVideo.tsx
+++ components/CardVideo.tsx
@@ -1,19 +1,25 @@
 import formatTime from '../utils/formatTime';
+import { format } from 'timeago.js';
 interface Props {
   urlThumbnail: string;
   title?: string;
   urlProfile?: string;
   duration: string;
+  createdAt: string;
 }
 
-const CardVideo = ({ urlThumbnail, title, duration }: Props) => {
+const CardVideo = ({ urlThumbnail, title, duration, createdAt }: Props) => {
   const regex = '[a-z]';
   return (
-    <div className="flex justify-center w-full relative">
-      <img src={urlThumbnail} alt="video thumbnail" className="w-full" />
-      <div className="absolute left-1.5 top-1 text-xs font-semibold bg-black/90">
-        {formatTime(duration)}
+    <div className="flex-col justify-center w-full">
+      <div className="relative">
+        <img src={urlThumbnail} alt="video thumbnail" className="w-full" />
+        <div className="absolute right-1 bottom-1 px-1 py-0.5 rounded text-xs font-semibold bg-black/90">
+          {formatTime(duration)}
+        </div>
       </div>
+      <h2 className="text-sm font-semibold pt-1">{title}</h2>
+      <h3 className="text-xs text-right text-gray-300">{format(createdAt)}</h3>
     </div>
   );
 };
--- pages/users/[userId].tsx
+++ pages/users/[userId].tsx
@@ -18,20 +18,22 @@ const User = () => {
   return (
     <Layout>
       <div className="grid grid-cols-[repeat(auto-fill,minmax(250px,_1fr))] gap-8 mx-auto p-4">
-        {data.data.map((video) => {
-          if (!video.thumbnail_url) {
+        {data.data.map(({ id, title, duration, thumbnail_url, created_at }) => {
+          if (!thumbnail_url) {
             // live video
             return null;
           }
-          const thumbnail = video.thumbnail_url
+          const thumbnail = thumbnail_url
             .replace('%{width}', '440')
             .replace('%{height}', '248');
 
           return (
             <CardVideo
-              key={video.id}
+              key={id}
+              title={title}
               urlThumbnail={thumbnail}
-              duration={video.duration}
+              duration={duration}
+              createdAt={created_at}
             />
           );
         })}
