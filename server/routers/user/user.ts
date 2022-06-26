c21dedb4e423e36e10180e72fd687821521bc2b3
Victor Garcia Mestre <vgmestre@gmail.com>
2022-06-26 21:56:38 +0200

    add video to watch later list api

--- server/routers/user/user.ts
+++ server/routers/user/user.ts
@@ -5,29 +5,72 @@ import { twitchEndpoints } from '../../../constants/twitchEndpoints';
 import { Videos } from '../../../types';
 import axios, { AxiosResponse } from 'axios';
 import { getAuthHeaders } from '../../../utils/getAuthHeaders';
+import { z } from 'zod';
 
-export const userRouter = createProtectedRouter().query('me', {
-  async resolve({ ctx }) {
-    const userId = ctx.session.user?.id;
-    if (!userId) {
-      throw new TRPCError({
-        code: 'BAD_REQUEST',
-        message: 'No user id',
+export const userRouter = createProtectedRouter()
+  .query('me', {
+    async resolve({ ctx }) {
+      const userId = ctx.session.user?.id;
+      if (!userId) {
+        throw new TRPCError({
+          code: 'BAD_REQUEST',
+          message: 'No user id',
+        });
+      }
+
+      const userData = await prisma.user.findFirst({
+        where: {
+          twitchId: ctx.session?.user?.id,
+        },
       });
-    }
+      if (!userData) {
+        throw new TRPCError({
+          code: 'NOT_FOUND',
+          message: `No user with id '${userId}'`,
+        });
+      }
 
-    const userData = await prisma.user.findFirst({
-      where: {
-        twitchId: ctx.session?.user?.id,
-      },
-    });
-    if (!userData) {
-      throw new TRPCError({
-        code: 'NOT_FOUND',
-        message: `No user with id '${userId}'`,
+      return userData;
+    },
+  })
+  .mutation('add-video', {
+    input: z.object({
+      videoId: z.string(),
+    }),
+    async resolve({ input, ctx }) {
+      const userId = ctx.session.user?.id;
+      if (!userId) {
+        throw new TRPCError({
+          code: 'BAD_REQUEST',
+          message: 'No user id',
+        });
+      }
+
+      // until prisma supports MongoDB's $addToSet:
+
+      const user = await prisma.user.findUnique({
+        where: {
+          twitchId: userId,
+        },
       });
-    }
 
-    return userData;
-  },
-});
+      if (!user) {
+        throw new TRPCError({
+          code: 'NOT_FOUND',
+          message: `No user with id '${userId}'`,
+        });
+      }
+
+      const videos = new Set(user.watchLater);
+      videos.add(input.videoId);
+
+      return prisma.user.update({
+        where: {
+          id: user.id,
+        },
+        data: {
+          watchLater: Array.from(videos),
+        },
+      });
+    },
+  });
